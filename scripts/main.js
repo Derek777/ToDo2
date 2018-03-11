fc80b5db856c66e3ecfd0ff11eace21af4d3a0e0
Derek777 <vvcompany777@gmail.com>
2018-03-11 21:47:23 +0200

    create new task function

--- scripts/main.js
+++ scripts/main.js
@@ -3,42 +3,23 @@ var inputTask = document.getElementById('new-task');
 var unfinishetTasks = document.getElementById('unfinishetTasks');
 
 
-function Task() {
+function Task(text) {
     var template = document.querySelector('#template');
     var clone = template.content.cloneNode(true);
-
-
-    this.addTask = function () {
-        parent = unfinishetTasks.appendChild(clone);
-    };
-
-    this.deleteTask = function () {
-        console.log(this);
-        // this
-       // clone.remove();
-        // var parent = self.parentNode;
-        // var li = parent.querySelector('.task');
-        // this.parentNode.parentNode.parentNode.querySelector('.task').remove();
-        // parent.removeChild('.task');
-    }.bind(unfinishetTasks);
-
-    clone.querySelector('.deleteBtn').onclick = this.deleteTask;
+    var val = document.createTextNode(text);
+    clone.querySelector('label').appendChild(val);
+    this.obj = clone;
 }
 
-// function deleteTask() {
-//     console.log("kffffff");
-// }
-
-// Task.prototype.delete = function () {
-//     this.delete();
-//     console.log(this);
-//     // this.parentNode.removeChild('.task');
-// };
+Task.prototype.newTask = function () {
+    unfinishetTasks.appendChild(this.obj);
+};
 
 function addTask() {
     if (inputTask.value){
-        var taskItem = new Task();
-        taskItem.addTask();
+        var taskItem = new Task(inputTask.value);
+        // console.log(taskItem);
+        taskItem.newTask();
         inputTask.value = "";
     } else {
         alert("no text");
